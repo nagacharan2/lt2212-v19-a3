ba2bb06f136b91a21de4a3977dc0214c14d8b4d0
NagaCharan Meda <nagacharan1990@gmail.com>
2019-03-25 17:55:47 +0100

    some changes

--- gendata.py
+++ gendata.py
@@ -6,20 +6,36 @@ import pandas as pd
 import re
 from nltk import word_tokenize
 
-
+#creates vocabulary
 def unique_words(text):
     uniq_words = {}    
     words = re.findall('\w+', text)
     uniq_words = set(words)
-    print(uniq_words)    
+#    print(uniq_words)    
     return uniq_words
 
+
+#removes all the POS tags
 def pre_process(file_path):
     text = []
     lines = open(file_path).read()#.split("\n")
     text = re.sub(r'/[^\s]+','',lines)
     return(text)
 
+
+#implements one hot encoding
+def one_hot_encoding(uniquewords):
+    one_hot_vectors = {}
+    
+    for num, word  in enumerate(uniquewords):
+        vector = [0] * len(uniquewords)
+        vector[num] = 1
+        one_hot_vectors[word] = vector
+    print(one_hot_vectors)
+    return one_hot_vectors
+
+#implement ngram model
+    
 
 # gendata.py -- Don't forget to put a reasonable amount code comments
 # in so that we better understand what you're doing when we grade!
@@ -45,6 +61,7 @@ args = parser.parse_args()
 print("Loading data from file {}.".format(args.inputfile))
 text = pre_process(args.inputfile)
 uniques = unique_words(text)
+one_hot = one_hot_encoding(uniques)
 print("Starting from line {}.".format(args.startline))
 if args.endline:
     print("Ending at line {}.".format(args.endline))
